c38cc6a20c667b7a58c779ff20434f18252341fc
Moésio Filho <moesiof.dev@gmail.com>
2021-01-18 20:07:46 -0300

    Adicionando classes: PermissaoEspecial e UtilsDebugMain
    - PermissaoEspecial com implementacao incial.
    - UtilsDebugMain serve para realizarmos testes internos e depurar as classes do pacote utils

---
+++ src/com/cooksbooks/utils/UtilsDebugMain.java
@@ -0,0 +1,16 @@
+package com.cooksbooks.utils;
+
+/**
+ * Classe feita para uso interno, não deve ser instanciada
+ * e nem utilizada em outras partes do código.
+ *
+ * Aqui podemos realizar testes para depurar
+ * o pacote com.cooksbooks.utils.
+ *
+ * @since 18-01-2021
+ */
+abstract class UtilsDebugMain {
+    public static void main(String[] args) {
+        PermissaoEspecial.debug();
+    }
+}
