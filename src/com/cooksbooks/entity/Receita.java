696f3ecbc33a20d270469bacb62ee806ee5ab2c2
Moésio Filho <moesiof.dev@gmail.com>
2021-01-19 21:27:03 -0300

    Implementação parcial de Receita e atualização em Relatorio
    - Receita: Alguns métodos e atributos foram adicionados. Ainda faltam alguns.
    - Relatorio: Reorganização da classe, novos comentários, getters/setters.

---
+++ src/com/cooksbooks/entity/Receita.java
@@ -0,0 +1,164 @@
+package com.cooksbooks.entity;
+
+import com.cooksbooks.utils.Ingredientes;
+import com.cooksbooks.utils.Custo;
+import com.cooksbooks.utils.Rendimento;
+import com.cooksbooks.utils.Dificuldade;
+import com.cooksbooks.utils.Comentario;
+import com.cooksbooks.utils.Imagem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A Classe Receita é responsável por representar uma receita no sistema.
+ *
+ * Ela possui diversos campos que a caracterizam e métodos para acessar e atualizar esses campos.
+ *
+ * @version 1.0
+ */
+public class Receita {
+    private String titulo;
+    private String modoPreparo;
+    // TODO: Adicionar private TempoPreparo tempoPreparo;
+    private Custo custo;
+    private Rendimento rendimento;
+    private Dificuldade dificuldade;
+    // TODO: Adicionar private List<Categoria> categoria;
+    private List<Ingredientes> ingredientes;
+    private List<Comentario> comentarios;
+    private List<Imagem> imagens;
+
+    // TODO: Adicionar getters/setters para os outros campos (titulo, tempoPreparo, custo, rendimento, dificuldade)
+
+    // Talvez adicionar outros construtores com algumas inforamçoes parciais
+    // Algum Receita(titulo, custo, rendimento, dificuldade)
+    // Ou melhor, usando as ideias do JavaBeans poderíamos ter um único construtor
+    // vazio Receita() e todos os campos seriam preenchidos por meio de getters/setters
+
+    /**
+     * Construtor nulo. Inicializa os campos com valores padrões.
+     * Valores padrão:
+     * Custo.BARATO, Rendimento.UM, Dificuldade.FACIL.
+     * Listas são inicializadas sem elementos.
+     * Strings são inicializadas como "".
+     */
+    public Receita() {
+        this.titulo = "";
+        this.modoPreparo = "";
+        this.custo = Custo.BARATO;
+        this.rendimento = Rendimento.UM;
+        this.dificuldade = Dificuldade.FACIL;
+        this.ingredientes = new ArrayList<>();
+        this.comentarios = new ArrayList<>();
+        this.imagens = new ArrayList<>();
+        // TODO: Inicializar tempoPreparo e categoria com valores padrão
+    }
+
+    /**
+     * Construtor que recebe um titulo.
+     * Demais elementos são inicializados com os valores padrões.
+     * @param titulo o título da receita.
+     */
+    public Receita(String titulo) {
+        this();
+        this.titulo = titulo;
+    }
+
+    /**
+     * Adiciona um novo comentário na lista de comentários.
+     * @param comentario o comentário a ser adicionado.
+     */
+    public void adicionarComentario(Comentario comentario) {
+        this.comentarios.add(comentario);
+    }
+
+    /**
+     * Remove um comentário da lista de comentários.
+     * @param comentario o comentário para ser removido.
+     * @return true se o comentário existia e foi removido, false caso contrário.
+     */
+    public boolean removerComentario(Comentario comentario) {
+        return this.comentarios.remove(comentario);
+    }
+
+    /* TODO: Implementar método para adicionar/remover uma Categoria
+    public void adicionarCategoria(Categoria categoria) { this.categorias.add(categoria) }
+
+    public boolean removerCategoria(Categoria categoria) { return this.categorias.remove(categoria);}
+     */
+
+    /**
+     * Adiciona um novo ingrediente na lista de ingredientes.
+     * @param ingrediente o ingrediente a ser adicionado.
+     */
+    public void adicionarIngrediente(Ingredientes ingrediente) {
+        this.ingredientes.add(ingrediente);
+    }
+
+    /**
+     * Remove um ingrediente da lista de ingredientes.
+     * @param ingrediente o ingrediente para ser removido.
+     * @return true se o ingrediente existia e foi removida, false caso contrário.
+     */
+    public boolean removerIngrediente(Ingredientes ingrediente) { return this.ingredientes.remove(ingrediente); }
+
+    /**
+     * Adiciona uma nova imagem na lista de imagens.
+     * @param imagem a imagem a ser adicionado.
+     */
+    public void adicionarImagem(Imagem imagem) {
+        this.imagens.add(imagem);
+    }
+
+    /**
+     * Remove uma imagem da lista de imagens.
+     * @param imagem a imagem para ser removido.
+     * @return true se a imagem existia e foi removida, false caso contrário.
+     */
+    public boolean removerImagem(Imagem imagem) {
+        return this.imagens.remove(imagem);
+    }
+
+    /* TODO: Implementar o método hashCode (depois revisar o "contrato" da API do Java)
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+     */
+
+    /* TODO: Implementar o método equals (depois revisar o "contrato" da API do Java)
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+     */
+
+    /**
+     * Retorna os principais campos do objeto receito de forma organizada.
+     * Principais campos: titulo, dificuldade, custo, rendimento, quantidades
+     * @return Retorna a representação do objeto receita em String
+     */
+    @Override
+    public String toString() {
+        return String.format("""
+                        %s
+                        Dificuldade: %s
+                        Custo: %s
+                        Rendimento: %s
+                        Quantidade Ingredientes: %d
+                        Quantidade Imagens: %d
+                        Quantidade Comentários: %d""",
+                        this.titulo, this.dificuldade, this.custo, this.rendimento,
+                        this.ingredientes.size(), this.imagens.size(), this.comentarios.size());
+    }
+
+    /**
+     * Função para uso interno, só deve ser utilizada para debug.
+     * Realiza algumas operações com a classe e imprime no console.
+     * */
+    public static void debug() {
+        Receita receita = new Receita();
+        System.out.println(receita);
+    }
+}
--- src/com/cooksbooks/entity/Relatorio.java
+++ src/com/cooksbooks/entity/Relatorio.java
@@ -1,6 +1,7 @@
 package com.cooksbooks.entity;
 
 import com.cooksbooks.utils.Comentario;
+
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
@@ -16,15 +17,30 @@ import java.util.List;
  * ao sistema um relátorio, que será retornado
  * como uma instância dessa classe.
  *
- * @version 1.0
+ * @version 1.1
  */
 public class Relatorio {
-    private final long qtdUsuariosCadastrados;
-    private final long qtdNovosUsuarios;
-    private final long qtdUsuariosAtivos;
-    private final LocalDate dataIncial;
-    private final LocalDate dataFinal;
-    private final List<Comentario> comentarios;
+    private long qtdUsuariosCadastrados;
+    private long qtdNovosUsuarios;
+    private long qtdUsuariosAtivos;
+    private LocalDate dataIncial;
+    private LocalDate dataFinal;
+    private List<Comentario> comentarios;
+
+    /**
+     * Construtor nulo. Inicializa os campos com valores padrões.
+     * Valores padrão:
+     * 0 para tipos númericos primitivos.
+     * null para tipos de referência.
+     */
+    public Relatorio() {
+        this.qtdUsuariosCadastrados = 0L;
+        this.qtdNovosUsuarios = 0L;
+        this.qtdUsuariosAtivos = 0L;
+        this.dataIncial = null;
+        this.dataFinal = null;
+        this.comentarios = null;
+    }
 
     /**
      * Construtor completo da classe.
@@ -40,8 +56,94 @@ public class Relatorio {
     }
 
     /**
+     * Retorna a quantiade de usuários cadastrados até a data final.
+     * @return Long com a quantidade de usuários cadastrados.
+     */
+    public long getQtdUsuariosCadastrados() {
+        return this.qtdUsuariosCadastrados;
+    }
+
+    /**
+     * Atualiza a quantiade de usuários cadastrados até a data final.
+     */
+    public void setQtdUsuariosCadastrados(long qtdUsuariosCadastrados) {
+        this.qtdUsuariosCadastrados = qtdUsuariosCadastrados;
+    }
+
+    /**
+     * Retorna a quantiade de novos usuários entre a data inicial e final.
+     * @return Long com a quantidade de novos usuários.
+     */
+    public long getQtdNovosUsuarios() {
+        return this.qtdNovosUsuarios;
+    }
+
+    /**
+     * Atualiza a quantiade de novos usuários entre a data inicial e final.
+     */
+    public void setQtdNovosUsuarios(long qtdNovosUsuarios) {
+        this.qtdNovosUsuarios = qtdNovosUsuarios;
+    }
+
+    /**
+     * Retorna a quantiade de usuários ativos entre a data inicial e final.
+     * @return Long com a quantidade de usuários ativos.
+     */
+    public long getQtdUsuariosAtivos() {
+        return this.qtdUsuariosAtivos;
+    }
+
+    /**
+     * Atualiza a quantiade de usuários ativos entre a data inicial e final.
+     */
+    public void setQtdUsuariosAtivos(long qtdUsuariosAtivos) {
+        this.qtdUsuariosAtivos = qtdUsuariosAtivos;
+    }
+
+    /**
+     * Retorna a data inicial do relatório.
+     * @return LocalDate com a data inicial do relatório,
+     */
+    public LocalDate getDataIncial() {
+        return this.dataIncial;
+    }
+
+    /**
+     * Define a data inicial do relatório.
+     */
+    public void setDataIncial(LocalDate dataIncial) {
+        this.dataIncial = dataIncial;
+    }
+
+    /**
+     * Retorna a data final do relatório.
+     * @return LocalDate com a data final do relatório,
+     */
+    public LocalDate getDataFinal() {
+        return this.dataFinal;
+    }
+
+    /**
+     * Define a data final do relatório.
+     */
+    public void setDataFinal(LocalDate dataFinal) {
+        this.dataFinal = dataFinal;
+    }
+
+    /**
+     * Retorna uma lista com os comentários contidos no relatório.
+     * @return {@code List<Comentario>} com os comentários contidos no relatório.
+     */
+    public List<Comentario> listarComentarios() {
+        return this.comentarios;
+    }
+
+    public void setComentarios(List<Comentario> comentarios) {
+        this.comentarios = comentarios;
+    }
+
+    /**
      * Calcula e retorna o percentual de usuários ativos.
-     *
      * @return Double que representa o percentual de usuários ativos.
      */
     public double percentualUsuariosAtivos() {
@@ -50,32 +152,11 @@ public class Relatorio {
 
     /**
      * Calcula e retorna o percentual do aumento de usuários.
-     *
      * @return Double que representa o percentual do aumento de usuários.
      */
     public double percentualAumentoUsuarios() {
         double qtdInicialUsuariosCadastrados = (this.qtdUsuariosCadastrados - this.qtdNovosUsuarios);
         return 100.0 * ((double) this.qtdNovosUsuarios / qtdInicialUsuariosCadastrados);
-    }
-
-    /**
-     * Retorna uma lista com os comentários contidos no relatório.
-     *
-     * @return {@code List<Comentario>} com os comentários contidos no relatório.
-     */
-    public List<Comentario> listarComentarios() {
-        return this.comentarios;
-    }
-
-    /**
-     * Retorna uma String que descreve o objeto (Data incial e data final)
-     * @return String no formato "Relatório de {dataInicial} até {dataFinal}"
-     */
-    @Override
-    public String toString() {
-        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        return String.format("Relatório de %s até %s", dateFormatter.format(this.dataIncial),
-                             dateFormatter.format(this.dataFinal));
     }
 
     /**
@@ -104,14 +185,32 @@ public class Relatorio {
     }
 
     /**
+     * Retorna uma String que descreve o objeto (Data incial e data final)
+     * @return String no formato "Relatório de {dataInicial} até {dataFinal}"
+     */
+    @Override
+    public String toString() {
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        return String.format("Relatório de %s até %s", dateFormatter.format(this.dataIncial),
+                             dateFormatter.format(this.dataFinal));
+    }
+
+    /**
      * Função para uso interno, só deve ser utilizada para debug.
      * Realiza algumas operações com a classe e imprime no console.
      * */
     public static void debug() {
-        LocalDate dataInicio = LocalDate.of(2021, 1, 1);
-        LocalDate dataFinal = LocalDate.of(2021, 1, 18);
-        Relatorio relatorio = new Relatorio(110L, 10L, 90L,
-                                            dataInicio, dataFinal, null);
+        Relatorio relatorio = new Relatorio();
+
+        relatorio.setDataIncial(LocalDate.of(2021, 1, 1));
+        relatorio.setDataFinal(LocalDate.of(2021, 1, 18));
+
+        relatorio.setQtdUsuariosCadastrados(110L);
+        relatorio.setQtdNovosUsuarios(10L);
+        relatorio.setQtdUsuariosAtivos(90L);
+
+        relatorio.setComentarios(null);
+
         relatorio.printRelatorio();
     }
 }
