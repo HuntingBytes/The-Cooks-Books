cbd03f21b84d0d515ef96286cf66b3e861755ea5
Eduardo Amorim <eduardoaamorim0@gmail.com>
2021-01-21 15:10:09 -0300

    Classe implementada: Administrador

--- src/com/cooksbooks/entity/Administrador.java
+++ src/com/cooksbooks/entity/Administrador.java
@@ -1,31 +1,42 @@
 package com.cooksbooks.entity;
 
 import com.cooksbooks.utils.PermissaoEspecial;
-
 import java.util.List;
 
-public class Administrador extends Perfil{
+public class Administrador extends Usuario {
 
-    public Administrador(String login, String senha){
+  private final List<PermissaoEspecial> permissoesAdministrador;
 
+  public Administrador(String login, String senha) {
+    super(login, senha);
+    this.permissoesAdministrador = PermissaoEspecial.gerarPermissoesPadrao();
+  }
+
+  public Administrador(String login, String senha, List<PermissaoEspecial> permissoes) {
+    super(login, senha);
+    this.permissoesAdministrador = PermissaoEspecial.gerarPermissoesPadrao();
+    this.permissoesAdministrador.addAll(permissoes);
+  }
+
+  public boolean possuiPermissao(PermissaoEspecial permissao) {
+      return permissoesAdministrador.contains(permissao);
+  }
+
+  public List<PermissaoEspecial> getPermissoes() {
+    return this.permissoesAdministrador;
+  }
+
+  public void adicionarPermissao(PermissaoEspecial permissao) {
+    if (!permissoesAdministrador.contains(permissao)) {
+      this.permissoesAdministrador.add(permissao);
     }
-    public Administrador(String login, String senha, List<PermissaoEspecial> permissoes){
+  }
 
+  public void removerPermissao(PermissaoEspecial permissao) {
+    if (permissoesAdministrador.contains(permissao)) {
+      this.permissoesAdministrador.remove(permissao);
     }
+  }
 
-    public boolean possuiPermissao(PermissaoEspecial permissao){
-        return false;
-    }
-
-    public List<PermissaoEspecial> getPermissoes(){
-        return null;
-    }
-
-    public void adicionarPermissao(PermissaoEspecial permissao){
-
-    }
-    public void removerPermissao(PermissaoEspecial permissao){
-
-    }
 
 }
