6d015f5ead86c7211348fd83277b336e15bad88c
Gabriel Pimentel <gabriel97pimentel@gmail.com>
2021-01-21 16:55:51 -0300

    Adicionando documentação à classe Administrador

--- src/com/cooksbooks/entity/Administrador.java
+++ src/com/cooksbooks/entity/Administrador.java
@@ -5,6 +5,10 @@ import java.util.List;
 
 public class Administrador extends Usuario {
 
+  /**
+   * A Classe Administrador representa um administrador do sistema, um usuário
+   * com permissões especiais, como REMOVER COMENTARIO e GERAR RELATORIO
+   */
   private final List<PermissaoEspecial> permissoesAdministrador;
 
   public Administrador(String login, String senha) {
@@ -18,25 +22,49 @@ public class Administrador extends Usuario {
     this.permissoesAdministrador.addAll(permissoes);
   }
 
+
+  /**
+   * Verifica se o Adm possui uma permissão especial específica
+   *
+   * @param permissao permissão a ser verificada
+   *
+   * @return boolean true: possui; false: não possui
+   */
   public boolean possuiPermissao(PermissaoEspecial permissao) {
       return permissoesAdministrador.contains(permissao);
   }
 
+
+  /**
+   * Retorna a lista de permissões de um Administrador
+   *
+   * @return List<PermissaoEspecial> lista de permissões
+   * */
   public List<PermissaoEspecial> getPermissoes() {
     return this.permissoesAdministrador;
   }
 
+
+  /**
+   * Adiciona uma permissão na lista de permissões do Administrador
+   *
+   * @param permissao permissão a ser adicionada
+   */
   public void adicionarPermissao(PermissaoEspecial permissao) {
     if (!permissoesAdministrador.contains(permissao)) {
       this.permissoesAdministrador.add(permissao);
     }
   }
 
+
+  /**
+   * Remove uma permissão na lista de permissões do Administrador
+   *
+   * @param permissao permissão a ser adicionada
+   */
   public void removerPermissao(PermissaoEspecial permissao) {
     if (permissoesAdministrador.contains(permissao)) {
       this.permissoesAdministrador.remove(permissao);
     }
   }
-
-
 }
