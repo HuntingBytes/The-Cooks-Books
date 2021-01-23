f9012fc5c48fb8068328068b68fa11067397df4c
Eduardo Amorim <eduardoaamorim0@gmail.com>
2021-01-23 15:39:21 -0300

    IRepositorioCaderno implementado, sem comentarios

--- src/com/cooksbooks/dados/interfaces/IRepositorioCaderno.java
+++ src/com/cooksbooks/dados/interfaces/IRepositorioCaderno.java
@@ -1,5 +1,23 @@
 package com.cooksbooks.dados.interfaces;
 
+import com.cooksbooks.entity.CadernoReceitas;
+import java.util.List;
+
 public interface IRepositorioCaderno {
 
+  void cadastrarCaderno(CadernoReceitas cadernoAdd);
+
+  void removerCaderno(String idCadernoRemover);
+
+  void alterarCadernoExistente(String idCadernoSubstituido, CadernoReceitas CadernoComAlteracoes);
+
+  boolean existeCaderno(String idCadernoExiste);
+
+  CadernoReceitas buscarCaderno(String idCadernoQueRetorna);
+
+  List<CadernoReceitas> buscarTodosCadernosDoUsuario(String nomeUsuario);
+
+  void salvarArquivo();
+
+  long quantidadeCadernosCadastrados();
 }
