8bf5a15067cb5f5cace38c392645ebf5847e007c
Eduardo Amorim <eduardoaamorim0@gmail.com>
2021-01-24 08:49:49 -0300

    implementaçao: ControladorCaderno sem comentarios

--- src/com/cooksbooks/controllers/ControladorCaderno.java
+++ src/com/cooksbooks/controllers/ControladorCaderno.java
@@ -1,5 +1,63 @@
 package com.cooksbooks.controllers;
 
+import com.cooksbooks.dados.interfaces.IRepositorioCaderno;
+import com.cooksbooks.entity.CadernoReceitas;
+import java.util.List;
+
 public class ControladorCaderno {
 
+  private final IRepositorioCaderno repositorioCadernos;
+
+  public ControladorCaderno(IRepositorioCaderno repositorioCadernos) {
+    this.repositorioCadernos = repositorioCadernos;
+  }
+
+  void cadastrarCaderno(CadernoReceitas caderno) {
+    if (cadernoValido(caderno)) {
+      caderno.setIdCaderno(String.format("%s-%d", caderno.getIdDono(),
+          repositorioCadernos.quantidadeCadernosCadastrados()));
+      repositorioCadernos.cadastrarCaderno(caderno);
+      repositorioCadernos.salvarArquivo();
+    }
+  }
+
+  void removerCaderno(String idCaderno) {
+    repositorioCadernos.removerCaderno(idCaderno);
+  }
+
+  public CadernoReceitas procurarCaderno(String idCaderno) {
+    return repositorioCadernos.buscarCaderno(idCaderno);
+  }
+
+  public List<CadernoReceitas> listarCadernosDoUsuario(String nomeUsuario) {
+    return repositorioCadernos.buscarTodosCadernosDoUsuario(nomeUsuario);
+  }
+
+  private boolean cadernoValido(CadernoReceitas caderno) {
+    return true;
+  }
+
+  public void alterarNomeCaderno(String idDoCaderno, String nomeNovo) {
+    if (nomeCadernoValido(nomeNovo)) {
+      repositorioCadernos.alterarNomeCadernoExistente(idDoCaderno, nomeNovo);
+    }
+  }
+
+  public void alterarInformacoesCaderno(String idDoCaderno, String informacoesNovas) {
+    if (informaçoesCadernoValido(informacoesNovas)) {
+      repositorioCadernos.alterarinformacoesCadernoExistente(idDoCaderno, informacoesNovas);
+    }
+  }
+
+  public void alterarCadernoPublico(String idDoCaderno) {
+    repositorioCadernos.alterarCadernoPublicoExistente(idDoCaderno);
+  }
+
+  private boolean nomeCadernoValido(String nome) {
+    return true;
+  }
+
+  private boolean informaçoesCadernoValido(String informacoes) {
+    return true;
+  }
 }
