5fc95a5f36c830053bd00718f10f330344229aa8
lucas-lins <lucaslins.link@gmail.com>
2021-01-25 19:07:43 -0300

    IRepoReceita e IRepoUsuario add

--- src/com/cooksbooks/dados/interfaces/IRepositorioReceita.java
+++ src/com/cooksbooks/dados/interfaces/IRepositorioReceita.java
@@ -1,5 +1,20 @@
 package com.cooksbooks.dados.interfaces;
 
+import com.cooksbooks.entity.Receita;
+import java.util.List;
+
 public interface IRepositorioReceita {
 
+    void salvarArquivo(); 
+
+    boolean existeReceita(Receita receitaExiste);
+
+    void cadastrarReceita(Receita receitaAdd);
+
+    void removerReceita(Receita receiraRemove);
+
+    Receita buscarReceita(Receita receitaBusca);
+
+    long totalReceitas();
+    
 }
--- src/com/cooksbooks/dados/interfaces/IRepositorioUsuario.java
+++ src/com/cooksbooks/dados/interfaces/IRepositorioUsuario.java
@@ -1,5 +1,18 @@
 package com.cooksbooks.dados.interfaces;
 
+import com.cooksbooks.entity.Usuario;
+import java.util.List;
+
 public interface IRepositorioUsuario {
+
+    void salvarArquivo();
+
+    boolean existeUsuario(Usuario usuarioExiste);
+
+    void cadastrarUsuario(Usuario usuarioAdd);
+
+    void removerUsuario(Usuario usuarioRemove);
+
+    Usuario buscarUsuario(Usuario usuarioBuscar);
 
 }
