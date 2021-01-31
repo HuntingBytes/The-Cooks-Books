c5ae9f02113f5fa6f363b9146e4c5536037273fc
Moésio Filho <moesiof.dev@gmail.com>
2021-01-31 09:45:15 -0300

    Adicionando atributos nos controladores e nomeando atributos nas views nos seguintes arquivos:
    - Java Classes: ControladorTelaCadastro, ControladorTelaLogin
    - FXML: TelaCadastro, TelaLogin

--- src/com/cooksbooks/gui/controllers/ControladorTelaCadastro.java
+++ src/com/cooksbooks/gui/controllers/ControladorTelaCadastro.java
@@ -1,5 +1,29 @@
 package com.cooksbooks.gui.controllers;
 
+import com.cooksbooks.utils.ExperienciaCulinaria;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+
 public class ControladorTelaCadastro {
+
+  @FXML
+  private TextField tfLogin;
+
+  @FXML
+  private TextField tfSenha;
+
+  @FXML
+  private TextField tfConfirmarSenha;
+
+  @FXML
+  private ChoiceBox<ExperienciaCulinaria> cbExperienciaCulinaria;
+
+  @FXML
+  private Button btCadastrar;
+
+  @FXML
+  private Button btVoltar;
 
 }
--- src/com/cooksbooks/gui/controllers/ControladorTelaLogin.java
+++ src/com/cooksbooks/gui/controllers/ControladorTelaLogin.java
@@ -1,5 +1,25 @@
 package com.cooksbooks.gui.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class ControladorTelaLogin {
+
+  @FXML
+  private TextField tfLogin;
+
+  @FXML
+  private PasswordField pfSenha;
+
+  @FXML
+  private Button btEntrar;
+
+  @FXML
+  private Button btCadastrar;
+
+  @FXML
+  private Button btAjuda;
 
 }
