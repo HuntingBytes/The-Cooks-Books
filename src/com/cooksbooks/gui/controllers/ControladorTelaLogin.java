387490c507a1735be700476553b10d8a5948c7d1
Moésio Filho <moesiof.dev@gmail.com>
2021-01-31 12:20:38 -0300

    Atualização em TelaLogin e ControladorTelaLogin
    - Nova label que serve para mostrar ao usuário algum erro no login
    - Novo alerta caso algum dos campos não seja preenchido
    - Atualização no controlador para adicionar essas funcionalidades

--- src/com/cooksbooks/gui/controllers/ControladorTelaLogin.java
+++ src/com/cooksbooks/gui/controllers/ControladorTelaLogin.java
@@ -2,10 +2,13 @@ package com.cooksbooks.gui.controllers;
 
 import com.cooksbooks.controllers.CooksBooksFachada;
 import com.cooksbooks.controllers.ICooksBooks;
+import java.util.ArrayList;
+import java.util.List;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
@@ -13,6 +16,9 @@ public class ControladorTelaLogin {
 
   private final ICooksBooks sistema = CooksBooksFachada.getInstancia();
   // private App app; Referência para a classe do aplicativo/programa
+
+  @FXML
+  private Label lbErro;
 
   @FXML
   private TextField tfLogin;
@@ -30,16 +36,36 @@ public class ControladorTelaLogin {
   private Button btAjuda;
 
   @FXML
+  public void initialize() {
+    this.tfLogin.textProperty().addListener((observableValue, s, t1) -> lbErro.setVisible(false));
+    this.pfSenha.textProperty().addListener((observableValue, s, t1) -> lbErro.setVisible(false));
+  }
+
+  @FXML
   private void handleEntrar() {
     String login = this.tfLogin.getText();
     String senha = this.pfSenha.getText();
 
-    if(login != null &&  senha != null) {
+    boolean loginBlank = login == null || login.isBlank();
+    boolean senhaBlank = senha == null || senha.isBlank();
+
+    if(!loginBlank && !senhaBlank) {
+      // Esse boolean de sistema.efetuarLogin() deverá ser
+      // trocado por blocos try-catch e sistema.efetuarLogin()
+      // não deve possuir retorno.
       if(sistema.efetuarLogin(login, senha)) {
-        System.out.println("Usuário logado!");
+        lbErro.setText("Usuário logado!");
+        lbErro.setVisible(true);
       } else {
-        System.out.println("Não logado.");
+        lbErro.setText("Usuário não logado.");
+        lbErro.setVisible(true);
       }
+    } else {
+      ArrayList<String> wrongFields = new ArrayList<>();
+      if (loginBlank) wrongFields.add("Login");
+      if (senhaBlank) wrongFields.add("Senha");
+
+      showCamposFaltando(wrongFields);
     }
   }
 
@@ -57,6 +83,33 @@ public class ControladorTelaLogin {
     alert.setContentText("Nessa tela você deve inserir seu login e senha e clicar no botão \"Entrar\"" +
                         "para conseguir acessar a plataforma!\n" +
                         "Caso ainda não possua uma conta, clique no botão \"Cadastrar\".");
+    alert.showAndWait();
+  }
+
+  private void showCamposFaltando(List<String> wrongFields) {
+    StringBuilder toPrint = new StringBuilder();
+    int size = wrongFields.size();
+
+    if (size > 1) {
+      toPrint.append("Favor, informar os campos ");
+    } else {
+      toPrint.append("Favor, informar o campo ");
+    }
+
+    for (int i = 0; i < size; i++) {
+      toPrint.append("\"");
+      toPrint.append(wrongFields.get(i));
+      toPrint.append("\"");
+      if (i < size - 1) {
+        toPrint.append(", ");
+      }
+    }
+
+    toPrint.append(".");
+
+    Alert alert = new Alert(Alert.AlertType.WARNING, toPrint.toString());
+    alert.setTitle("Campos não preenchidos!");
+    alert.setHeaderText("");
     alert.showAndWait();
   }
 
