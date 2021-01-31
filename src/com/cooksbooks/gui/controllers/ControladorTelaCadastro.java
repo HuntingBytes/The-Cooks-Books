ab929bcab7d2e72947fd08517902ddbfd944e293
Moésio Filho <moesiof.dev@gmail.com>
2021-01-31 10:47:48 -0300

    Implementação inicial: Login + Cadastro (Telas e Controladores)
    - Funções iniciais para cadastrar e realizar login;
    - Funções para mudança de telas não implementada;

--- src/com/cooksbooks/gui/controllers/ControladorTelaCadastro.java
+++ src/com/cooksbooks/gui/controllers/ControladorTelaCadastro.java
@@ -1,5 +1,8 @@
 package com.cooksbooks.gui.controllers;
 
+import com.cooksbooks.controllers.CooksBooksFachada;
+import com.cooksbooks.controllers.ICooksBooks;
+import com.cooksbooks.entity.Usuario;
 import com.cooksbooks.utils.ExperienciaCulinaria;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -7,6 +10,9 @@ import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
 
 public class ControladorTelaCadastro {
+
+  private final ICooksBooks sistema = CooksBooksFachada.getInstancia();
+  // private App app; Referência para a classe do aplicativo/programa
 
   @FXML
   private TextField tfLogin;
@@ -25,5 +31,40 @@ public class ControladorTelaCadastro {
 
   @FXML
   private Button btVoltar;
+
+  @FXML
+  public void initialize() {
+    this.cbExperienciaCulinaria.getItems().addAll(ExperienciaCulinaria.values());
+  }
+
+  @FXML
+  private void handleCadastrar() {
+    String login = tfLogin.getText();
+    String senha = tfSenha.getText();
+    String confirmarSenha = tfConfirmarSenha.getText();
+    ExperienciaCulinaria experienciaCulinaria = cbExperienciaCulinaria.getValue();
+
+    if(login != null && senha != null && confirmarSenha != null && experienciaCulinaria != null) {
+      if(senha.equals(confirmarSenha)) {
+        Usuario usuario = new Usuario(login, senha);
+        usuario.setNomePerfil(login);
+        usuario.setBiografia("");
+        usuario.setExperienciaCulinaria(experienciaCulinaria);
+
+        sistema.cadastrarUsuario(usuario);
+
+        System.out.println("Usuário cadastrado!");
+      } else {
+        System.out.println("Senhas não coincidem!");
+      }
+    }
+
+  }
+
+  @FXML
+  private void handleVoltar() {
+    // Usa o this.app para atualizar a tela, talvez?
+    // Troca para a tela de cadastro
+  }
 
 }
--- src/com/cooksbooks/gui/controllers/ControladorTelaLogin.java
+++ src/com/cooksbooks/gui/controllers/ControladorTelaLogin.java
@@ -1,11 +1,18 @@
 package com.cooksbooks.gui.controllers;
 
+import com.cooksbooks.controllers.CooksBooksFachada;
+import com.cooksbooks.controllers.ICooksBooks;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
 public class ControladorTelaLogin {
+
+  private final ICooksBooks sistema = CooksBooksFachada.getInstancia();
+  // private App app; Referência para a classe do aplicativo/programa
 
   @FXML
   private TextField tfLogin;
@@ -21,5 +28,42 @@ public class ControladorTelaLogin {
 
   @FXML
   private Button btAjuda;
+
+  @FXML
+  private void handleEntrar() {
+    String login = this.tfLogin.getText();
+    String senha = this.pfSenha.getText();
+
+    if(login != null &&  senha != null) {
+      if(sistema.efetuarLogin(login, senha)) {
+        System.out.println("Usuário logado!");
+      } else {
+        System.out.println("Não logado.");
+      }
+    }
+  }
+
+  @FXML
+  private void handleCadastrar() {
+    // Usa o this.app para atualizar a tela, talvez?
+    // Troca para a tela de cadastro
+  }
+
+  @FXML
+  private void handleAjuda() {
+    Alert alert = new Alert(AlertType.INFORMATION);
+    alert.setTitle("Ajuda - Tela de Login");
+    alert.setHeaderText("");
+    alert.setContentText("Nessa tela você deve inserir seu login e senha e clicar no botão \"Entrar\"" +
+                        "para conseguir acessar a plataforma!\n" +
+                        "Caso ainda não possua uma conta, clique no botão \"Cadastrar\".");
+    alert.showAndWait();
+  }
+
+  /* Seta a instância do aplicativo
+  public void setApp(App app) {
+    this.app = app;
+  }
+  */
 
 }
