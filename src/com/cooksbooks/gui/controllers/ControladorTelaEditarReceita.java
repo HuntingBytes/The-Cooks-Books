167bda45f6f405df9bca30d7641ceea9118f9c9f
lucas-lins <lucaslins.link@gmail.com>
2021-02-09 19:47:16 -0300

    adicionando fxml e controlador de EditarReceita

--- src/com/cooksbooks/gui/controllers/ControladorTelaEditarReceita.java
+++ src/com/cooksbooks/gui/controllers/ControladorTelaEditarReceita.java
@@ -1,5 +1,113 @@
 package com.cooksbooks.gui.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.Separator;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class ControladorTelaEditarReceita {
-    //Necessário implementar
+
+    //Apenas Esqueleto Simples
+
+    @FXML
+    private TextField tfAlterarNome;
+
+    @FXML
+    private Button btConfirmarNome;
+
+    @FXML
+    private Button btCancelarNome;
+
+    @FXML
+    private Label lbAlterarNome;
+
+    @FXML
+    private ChoiceBox<?> cbTempoDePreparo;
+
+    @FXML
+    private Label lbTempoDePreparo;
+
+    @FXML
+    private Button btConfirmarTempo;
+
+    @FXML
+    private Button btCancelarTempo;
+
+    @FXML
+    private Label lbCustoMedio;
+
+    @FXML
+    private ChoiceBox<?> cbCustoMedio;
+
+    @FXML
+    private Button btConfirmarCusto;
+
+    @FXML
+    private Button btCancelarCusto;
+
+    @FXML
+    private Label lbRendimento;
+
+    @FXML
+    private ChoiceBox<?> cbRendimento;
+
+    @FXML
+    private Button btConfirmarRendimento;
+
+    @FXML
+    private Button btCancelarRendimento;
+
+    @FXML
+    private Label lbDificuldade;
+
+    @FXML
+    private ChoiceBox<?> cbDificuldade;
+
+    @FXML
+    private Button btConfirmarDificuldade;
+
+    @FXML
+    private Button brCancelarDificuldade;
+
+    @FXML
+    private Separator spSeparadorVertical;
+
+    @FXML
+    private Label lbAlterarModoPreparo;
+
+    @FXML
+    private TextArea taAlterarModo;
+
+    @FXML
+    private Button btConfirmarModo;
+
+    @FXML
+    private Button btCancelarModo;
+
+    @FXML
+    private Label lbAlterarCategoria;
+
+    @FXML
+    private TextArea tfAlterarCategoria;
+
+    @FXML
+    private Button btConfirmarCategoria;
+
+    @FXML
+    private Button btCancelarCategoria;
+
+    @FXML
+    private Separator spSeparadorHorizontal;
+
+    @FXML
+    private Button btGeralConfirmar;
+
+    @FXML
+    private Button btGeralReverter;
+
+    @FXML
+    private Button btGeralVoltar;
 }
