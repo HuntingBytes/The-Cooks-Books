43fd4c1cb3a25693f50e9407476b84ac693684fb
Moésio Filho <moesiof.dev@gmail.com>
2021-02-17 18:42:04 -0300

    Implementação Inicial dos controladores do Administrador
    - Atualização nos FXML e Controladores

--- src/com/cooksbooks/gui/controllers/ControladorAdm.java
+++ src/com/cooksbooks/gui/controllers/ControladorAdm.java
@@ -1,33 +1,118 @@
 package com.cooksbooks.gui.controllers;
 
+import com.cooksbooks.controllers.CooksBooksFachada;
+import com.cooksbooks.controllers.ICooksBooks;
+import com.cooksbooks.entity.Receita;
+import com.cooksbooks.entity.Usuario;
+import com.cooksbooks.exceptions.UsuarioInexistente;
+import com.cooksbooks.gui.ScreenManager;
+import java.io.IOException;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 
 public class ControladorAdm {
 
-    @FXML
-    private TextField tfBarraPesquisa;
+  private final ICooksBooks sistema = CooksBooksFachada.getInstancia();
 
-    @FXML
-    private ChoiceBox<?> cbOpcoesPesquisa;
+  @FXML
+  private TextField tfBarraPesquisa;
+  @FXML
+  private Button btPesquisar;
+  @FXML
+  private ListView<Usuario> lvResultadoPesquisa;
+  @FXML
+  private Button btGerarRelatorio;
+  @FXML
+  private Button btBanirUsuario;
+  @FXML
+  private Button btVisualizarReceitas;
+  @FXML
+  private Button btVisualizarCadernos;
 
-    @FXML
-    private ListView<?> lvResultadoPesquisa;
-
-    @FXML
-    private Button btGerarRelatorio;
-
-    @FXML
-    private Button btBanirUsuario;
-
-    @FXML
-    private Button btVisualizarReceitas;
-
-    @FXML
-    private Button btVisualizarCadernos;
-
+  @FXML
+  public void initialize() {
+    this.lvResultadoPesquisa.setPlaceholder(new Label("Digite um login de usuário e clique em \"Pesquisar\"."));
+    this.tfBarraPesquisa.textProperty().addListener((observableValue, s, t1) -> {
+      lvResultadoPesquisa.getItems().clear();
+      lvResultadoPesquisa.refresh();
+    });
   }
+
+  @FXML
+  private void handlePesquisar() {
+    if (isBuscaValida()) {
+      String busca = this.tfBarraPesquisa.getText();
+      try {
+        Usuario u = this.sistema.buscarUsuario(busca);
+        ObservableList<Usuario> listUsuarios = FXCollections.observableArrayList(u);
+        this.lvResultadoPesquisa.setItems(listUsuarios);
+      } catch (UsuarioInexistente uInexistente) {
+        System.out.println(uInexistente.getMessage());
+        this.lvResultadoPesquisa.setPlaceholder(new Label("Nenhum usuário com esse login encontrado."));
+      }
+    }
+  }
+
+  @FXML
+  private void handleGerarRelatorio() {
+    try {
+      ScreenManager.getInstancia().abrirTelaRelatorio();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  @FXML
+  private void handleBanirUsuario() {
+    Usuario usuarioSelecionado = lvResultadoPesquisa.getSelectionModel().getSelectedItem();
+    if (usuarioSelecionado != null) {
+      try {
+        this.sistema.removerUsuario(usuarioSelecionado.getLogin());
+        clearCampos();
+      } catch (UsuarioInexistente usuarioInexistente) {
+        usuarioInexistente.printStackTrace();
+      }
+    }
+  }
+
+  @FXML
+  private void handleVisualizarReceitas() {
+    Usuario usuarioSelecionado = lvResultadoPesquisa.getSelectionModel().getSelectedItem();
+    if (usuarioSelecionado != null) {
+      try {
+        ScreenManager.getInstancia().abrirTelaReceitaRelatorio(usuarioSelecionado.getLogin());
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+  }
+
+  @FXML
+  private void handleVisualizarCadernos() {
+    Usuario usuarioSelecionado = lvResultadoPesquisa.getSelectionModel().getSelectedItem();
+    if (usuarioSelecionado != null) {
+      try {
+        ScreenManager.getInstancia().abrirTelaCadernoRelatorio(usuarioSelecionado.getLogin());
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+  }
+
+  private boolean isBuscaValida() {
+    return this.tfBarraPesquisa.getText() != null && !this.tfBarraPesquisa.getText().isBlank();
+  }
+
+  private void clearCampos() {
+    this.tfBarraPesquisa.setText("");
+    this.lvResultadoPesquisa.getItems().clear();
+    this.lvResultadoPesquisa.refresh();
+  }
+}
 
--- src/com/cooksbooks/gui/controllers/ControladorCadernoRelatorio.java
+++ src/com/cooksbooks/gui/controllers/ControladorCadernoRelatorio.java
@@ -1,17 +1,64 @@
 package com.cooksbooks.gui.controllers;
 
+import com.cooksbooks.controllers.CooksBooksFachada;
+import com.cooksbooks.controllers.ICooksBooks;
+import com.cooksbooks.entity.CadernoReceitas;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.stage.Stage;
 
 public class ControladorCadernoRelatorio {
 
-  @FXML
-  private ListView<?> lvCadernosUsuarioRelatorio;
+  private final ICooksBooks sistema = CooksBooksFachada.getInstancia();
 
   @FXML
-  private Button btRemoverCadernoRelatorio;
+  private ListView<CadernoReceitas> lvCadernosUsuario;
 
   @FXML
-  private Button btFecharCadernoRelatorio;
+  private Button btRemoverCaderno;
+
+  @FXML
+  private Button btFechar;
+
+  @FXML
+  private Label lbTitulo;
+
+  @FXML
+  private void handleRemoverItem() {
+    CadernoReceitas caderno = this.lvCadernosUsuario.getSelectionModel().getSelectedItem();
+    int indiceCaderno = this.lvCadernosUsuario.getSelectionModel().getSelectedIndex();
+    if (caderno != null) {
+      try {
+        sistema.removerCaderno(caderno.getIdCaderno());
+        this.lvCadernosUsuario.getItems().remove(indiceCaderno);
+        this.lvCadernosUsuario.refresh();
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    }
+  }
+
+  @FXML
+  private void handleFechar() {
+    ((Stage) this.btFechar.getScene().getWindow()).close();
+    this.clearCampos();
+  }
+
+  private void clearCampos() {
+    this.lbTitulo.setText("Caderno(s) de");
+    this.lvCadernosUsuario.getItems().clear();
+    this.lvCadernosUsuario.refresh();
+  }
+
+  public void inicializar(String loginUsuario) {
+    this.lbTitulo.setText(String.format("Caderno(s) de %s", loginUsuario));
+    ObservableList<CadernoReceitas> cadernoReceitas =
+        FXCollections.observableList(sistema.buscarTodosCadernosDoUsuario(loginUsuario));
+    this.lvCadernosUsuario.setItems(cadernoReceitas);
+  }
+
 }
