5439eb5a4553aa56c672df13fdb43ee48f9fb17f
Moésio Filho <moesiof.dev@gmail.com>
2021-02-18 14:23:47 -0300

    Atualizando o pacote com.cooksbooks.exceptions com o formato do Google
    - Nenhuma mudança lógica nas classes.

--- src/com/cooksbooks/exceptions/CadernoInexistente.java
+++ src/com/cooksbooks/exceptions/CadernoInexistente.java
@@ -1,15 +1,18 @@
 package com.cooksbooks.exceptions;
 
 /**
- * Essa classe representa a exceção no qual existe
- * uma tentativa de manipular um caderno que não existe.
+ * Essa classe representa a exceção no qual existe uma tentativa de manipular um caderno que não
+ * existe.
+ *
  * @version 1.0
  */
 public class CadernoInexistente extends Exception {
+
   private final String idCaderno;
 
   /**
    * Construtor que recebe o id que não existe.
+   *
    * @param idCaderno ID do caderno que não existe.
    */
   public CadernoInexistente(String idCaderno) {
--- src/com/cooksbooks/exceptions/UsuarioJaCadastrado.java
+++ src/com/cooksbooks/exceptions/UsuarioJaCadastrado.java
@@ -1,16 +1,18 @@
 package com.cooksbooks.exceptions;
 
 /**
- * Essa classe representa a exceção no qual existe
- * uma tentativa de cadastrar um usuário que já existe
- * no repositório. (i. e., login já está sendo utilizado)
+ * Essa classe representa a exceção no qual existe uma tentativa de cadastrar um usuário que já
+ * existe no repositório. (i. e., login já está sendo utilizado)
+ *
  * @version 1.0
  */
 public class UsuarioJaCadastrado extends Exception {
+
   private final String loginUsuario;
 
   /**
    * Construtor que recebe o login do usuário tentando ser cadastrado.
+   *
    * @param loginUsuario Login do usuário tentando ser cadastrado.
    */
   public UsuarioJaCadastrado(String loginUsuario) {
